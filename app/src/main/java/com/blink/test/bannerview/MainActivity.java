a50e0bca7e509d60df2583ed4812613a578e7e51
daiyanfeng@vip.qq.com <daiyanfeng@vip.qq.com>
2018-11-02 20:54:42 +0800

    test

--- app/src/main/java/com/blink/test/bannerview/MainActivity.java
+++ app/src/main/java/com/blink/test/bannerview/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         viewList = new ArrayList<View>();
         for (int i = 0; i < imgs.length; i++) {
             ImageView image = new ImageView(this);
